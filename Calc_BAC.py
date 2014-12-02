0d019ae8943787f239f98bd7457f4a279efdf92c
No Name Yet <kg8@rice.edu>
2014-12-02 14:07:41 -0600

    Finalized code

--- Calc_BAC.py
+++ Calc_BAC.py
@@ -6,9 +6,17 @@ import re
 
 # Function to calculate BAC ----------------------------------------------
 
-def calc_BAC(n): # Calculates BAC based on physical info and # beers consumed
+def calc_BAC(a,b,c): # Calculates BAC based on physical info and # beers consumed
 
-    raw_text=n
+#a--text from DL magnetic strip
+#b-- ABV for the current beer
+#c-- Pour volume (in oz.)
+
+    ABV=b
+
+    pour_vol=c
+    
+    raw_text=a
     text=raw_text.replace('\'', ' ').replace('\"', ' ').replace('\*', ' ')
 
 #Parsing text to identify weight and gender
@@ -47,17 +55,19 @@ def calc_BAC(n): # Calculates BAC based on physical info and # beers consumed
 ## Calculating the BAC for the individual
 
     swipes=open("swipes.txt", 'r')
-   
-    BAC=0
-    beer_size=16/12
 
+    beer_alc_ratio=(pour_vol/12)*(ABV/0.05)   
+
+    BAC=0
+    
     for line in swipes:
         if re.search(DL_num, line, re.IGNORECASE):
             unix_time=line.split(' ')[-1]
 
             time_diff_hrs=(time.time()-float(unix_time))/3600
-            
-            contributing_BAC=(Body_H2O*g_const*beer_size)/(weight*gen_const)-t_const*time_diff_hrs
+
+        ## Formula for calculating BAC
+            contributing_BAC=(Body_H2O*g_const*beer_alc_ratio)/(weight*gen_const)-t_const*time_diff_hrs
 
             if contributing_BAC >0:
                     BAC=BAC + contributing_BAC
