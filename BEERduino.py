c2a12d01c0e559954e30f2460739c3b3a3ee1f9a
Lucas Hartsough <lucash@rice.edu>
2014-12-25 21:41:02 -0800

    OOP Changes: added DLicense class

--- BEERduino.py
+++ BEERduino.py
@@ -29,6 +29,125 @@ class color:
     GREEN= '\033[92m'
     RED='\033[91m'
     
+class DLicense():
+    '''License object parses input raw text from card reader and holds the appropriate variables.'''
+    def __init__(self, raw_text):
+        '''Parses raw_text and populates variables.'''
+        # vars
+        self.isValid = False
+        self.num = None
+        self.firstName = None
+        self.lastName = None
+        # Parse input text
+        text=raw_text.replace('\'', ' ').replace('\"', ' ').replace('\*', ' ')
+        # Check to make sure the swiped card is a drivers license and obtain DL number
+        check_text=text.split(';')[1]
+        check_num=int(check_text[0:4]) # Indicator of drivers' licenses
+        if check_num==6360:
+            self.isValid=True
+        else:
+            self.isValid=False
+            print 'Try swiping a drivers license'
+        self.num=check_text[6:14] #Save DL number for identification
+        # Parsing license info to determine validity of license and age of user
+        dates_text=text.split('=')[1]
+        # Separate out dates by year, month, and day
+        self.exp_yr=int(dates_text[0:2])
+        self.exp_mo=int(dates_text[2:4])
+        self.br_yr=int(dates_text[4:8])
+        self.br_mo=int(dates_text[8:10])
+        self.br_dt=int(dates_text[10:12])
+        # Identifying the user's  name
+        name_text=text.split('^')[1]
+        names=name_text.replace('$', ' ').split(' ') # some DLs use $ and spaces in names
+        self.firstName=names[0]
+        self.lastName=names[-1]
+        print color.BOLD + self.firstName, " ", self.lastName + color.END
+        #Parsing text to identify weight and gender
+        phys_text=text.split('+') # Remove first half of code
+        phys_text=re.sub(' ','',phys_text[1]) #Removing extra spaces
+        phys_text=re.split('(\d+)',phys_text,flags=re.IGNORECASE) # split out whenever letters and numbers are together
+        phys_info=phys_text[3] # This set of numbers contains gender, height, and weight
+        # Assigning weight obtained from DL
+        weight_lb=int(phys_info[4:7])
+        self.weight=weight_lb/2.2
+        # Assigning gender constant for BAC fomula
+        if int(phys_info[0])==1 or 'F':
+            self.gender = 'F'
+        elif int(phys_info[0])==2 or 'M':
+            self.gender = 'M'
+        
+    def checkLicense(self):
+        '''Ensures swiped card is a license. Also confirms expiration and determines if the user is of age.
+        Output modifies self.isValid to be correct.'''
+        # Current time
+        timestr = time.strftime("%Y%m%d%H%M%S")
+        cur_yr=int(timestr[0:4]) #Using 4 digit year
+        cur_yr_2=int(timestr[2:4]) #Using 2 digit year
+        cur_mo=int(timestr[4:6])
+        cur_dt=int(timestr[6:8])
+        # check to see that license is still valid
+        expired_msg = 'Sorry your license has expired!'
+        if self.isValid:
+            if cur_yr_2>self.exp_yr:
+                print expired_msg
+                self.isValid=False
+            elif cur_yr_2==self.exp_yr:
+                if cur_mo>self.exp_mo:
+                        print expired_msg
+                        self.isValid=False
+    
+        #Check to see if the user is over 21
+        under_21_msg= 'You appear to be under 21 - no beer for you!'
+        if self.isValid:
+            if cur_yr-self.br_yr<21:
+                self.isValid=False
+                print under_21_msg
+            elif cur_yr-self.br_yr==21:
+                if cur_mo<self.br_mo:
+                    self.isValid=False
+                    print under_21_msg
+                elif cur_mo==self.br_mo:
+                    if cur_dt<self.br_dt:
+                        self.isValid=False
+                        print under_21_msg
+                    elif cur_dt==self.br_dt:
+                        print 'Happy 21st Birthday!'
+                        
+def calc_BAC(DL,ABV,pour_vol):
+    '''Calculates BAC based on physical info.
+    Inputs:
+        dl (DLicense obj) - driver license object
+        ABV (float) - ABV of the beer on tap
+        pour_vol (float) - volume of a single pour (probably 16oz)
+    Returns:
+        BAC (float)'''
+    # Standard BAC constants 
+    Body_H2O=0.806
+    g_const=1.2
+    t_const=0.015
+
+    # Assigning gender constant for BAC fomula
+    if DL.gender == 'F':
+        gen_const= 0.58
+    elif DL.gender == 'M':
+        gen_const= 0.49
+      
+    ## Calculating the BAC for the individual
+    swipes=open("swipes.txt", 'r')
+    beer_alc_ratio=(pour_vol/12)*(ABV/0.05)   
+    BAC=0
+    for line in swipes:
+        if re.search(DL.num, line, re.IGNORECASE):
+            unix_time=line.split(' ')[-1]
+            time_diff_hrs=(time.time()-float(unix_time))/3600.0
+            ## Formula for calculating BAC
+            contributing_BAC=(Body_H2O*g_const*beer_alc_ratio)/(DL.weight*gen_const)-t_const*time_diff_hrs
+            if contributing_BAC >0:
+                    BAC=BAC + contributing_BAC
+    swipes.close()
+    return BAC
+    
 # Setting a while loop to run continuously and a try statement for error handling------------------
 while True:
     try:
@@ -60,19 +179,17 @@ while True:
                 
                 # Send text from magnetic strip swipe to the function 'check_license' and collect output
                 raw_text=getpass.getpass(color.GREEN + 'Swipe card now:   ' + color.END).strip() 
-                DLicense=check_license(raw_text)
+                dl = DLicense(raw_text)
                 
                 # Check to see if the user is a registered user
                 users=open("users_list.txt", 'r') # use defined users list
-                hit=0 #random counter for matching DL line by line
-                       
-                if DLicense['isValid']:
+                if dl.isValid:
                     for line in users:
-                        if DLicense['DL_num']in line:
-                            DLicense['isValid'] = True
+                        if dl.num in line:
+                            dl.isValid = True
                         else: 
                             print 'Not registered user'
-                            DLicense['isValid']=False
+                            dl.isValid=False
                 users.close()
                 
                 # Calculating the user's current BAC
